695bb84d226efebe364696c03b29a28fd068f534
费旋 <501331651@qq.com>
2026-06-09 17:01:24 +0800

    换端口

--- electron/main.js
+++ electron/main.js
@@ -29,7 +29,7 @@ function createWindow() {
   })
 
   if (isDev) {
-    mainWindow.loadURL('http://localhost:')
+    mainWindow.loadURL('http://localhost:5188')
     mainWindow.webContents.openDevTools({ mode: 'detach' })
   } else {
     mainWindow.loadFile(path.join(__dirname, '../dist/index.html'))
